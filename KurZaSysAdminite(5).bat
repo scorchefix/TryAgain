c5c416bbe0c3dfd82bf01d6787dca5a20932af8b
scorchefix <scorchefixx@abv.bg>
2018-07-19 16:19:10 +0300

    Add files via upload
    // Removed Adobe Acrobat Auto Update service

---
+++ KurZaSysAdminite(5).bat
@@ -0,0 +1,12 @@
+$echo off
+reg delete HKLM\Software\Policies\Google\Chrome /v HomepageLocation /f
+reg delete HKLM\Software\Policies\Google\Chrome\RestoreOnStartupURLs /v 1 /f
+reg delete HKLM\Software\Policies\Microsoft\Windows\Personalization /v LockScreenImage /f
+reg delete HKLM\Software\Policies\Microsoft\Windows\Personalization /v NoChangingLockScreen /f
+reg add HKLM\Software\Policies\Microsoft\WindowsFirewall\DomainProfile\ /v EnableFirewall /t REG_DWORD /d 1 /f
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies /v NoChangingWallPaper /f
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System /f
+reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Policies\Microsoft\FVE /v RDVDenyWriteAccess /t REG_SZ /d 0 /f
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableBkGndGroupPolicy  /t REG_SZ /d 1 /f
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome /v RestoreOnStartup /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Adobe\Adobe ARM\Legacy\Reader\{AC76BA86-7AD7-1033-7B44-AC0F074E4100}" /v Mode /t REG_DWORD /d 0 /f
